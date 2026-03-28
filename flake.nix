b794da3ffb2110b36de8e3facad49ac1444a2c16
cmspam <cmspam>
2026-03-29 00:20:34 +0900

    feat: migrate to QEMU git-master with dynamic versioning

--- flake.nix
+++ flake.nix
@@ -1,25 +1,79 @@
 {
-  description = "QEMU with VAAPI hardware acceleration and latency tweaks";
+  description = "QEMU from git master with PAT patch";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    qemu-src = {
+      url = "git+https://gitlab.com/qemu-project/qemu.git";
+      flake = false;
+    };
+
+    keycodemapdb = {
+      url = "git+https://gitlab.com/qemu-project/keycodemapdb.git";
+      flake = false;
+    };
+    berkeley-softfloat-3 = {
+      url = "git+https://gitlab.com/qemu-project/berkeley-softfloat-3.git";
+      flake = false;
+    };
+    berkeley-testfloat-3 = {
+      url = "git+https://gitlab.com/qemu-project/berkeley-testfloat-3.git";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, qemu-src, ... }@inputs:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
       overlays.default = final: prev: {
-        # We use 'prev.qemu' to grab the original, then 'overrideAttrs' to change it
         qemu = prev.qemu.overrideAttrs (oldAttrs: {
-          pname = "qemu-patched";
+          pname = "qemu-patched-git";
+          
+          # DYNAMIC VERSIONING:
+          # This creates a version like: "10.2.91-git-20260329-770f50c"
+          # It uses the base version, the date of the commit, and the short hash.
+          version = "${nixpkgs.lib.substring 0 8 qemu-src.lastModifiedDate}-${qemu-src.shortRev or "dirty"}";
 
-          # The missing semicolon was here!
-          patches = (oldAttrs.patches or [ ]) ++ [
+          src = qemu-src;
+
+          patches = [
             ./qemu-all-in-one.patch
           ];
+
+          preConfigure = ''
+            touch .git
+            mkdir -p subprojects
+            
+            cp -a ${inputs.keycodemapdb} subprojects/keycodemapdb
+            cp -a ${inputs.berkeley-softfloat-3} subprojects/berkeley-softfloat-3
+            cp -a ${inputs.berkeley-testfloat-3} subprojects/berkeley-testfloat-3
+            chmod -R +w subprojects/
+
+            cp -r subprojects/packagefiles/berkeley-softfloat-3/* subprojects/berkeley-softfloat-3/
+            cp -r subprojects/packagefiles/berkeley-testfloat-3/* subprojects/berkeley-testfloat-3/
+            
+            rm -rf build/meson-private
+
+          '';
+
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
+            final.python3Packages.setuptools
+            final.python3Packages.wheel
+            final.python3Packages.pip
+            final.git
+            final.meson
+            final.ninja
+          ];
+
+          configureFlags = (oldAttrs.configureFlags or [ ]) ++ [
+            "--disable-download"
+          ];
+
+          doCheck = false;
         });
       };
 
@@ -32,9 +86,7 @@
           };
         in
         {
-          # Now 'pkgs.qemu' refers to your patched version
           default = pkgs.qemu;
-          qemu-patched = pkgs.qemu;
         });
 
       devShells = forAllSystems (system:
@@ -43,7 +95,6 @@
         in
         {
           default = pkgs.mkShell {
-            # This puts your patched QEMU into the PATH of the shell
             buildInputs = [ pkgs.qemu ];
           };
         });
