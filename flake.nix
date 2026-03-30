65b59ea1826fb2dba4e8de491f0828bc309a86e2
cmspam <charlesmillerspam@gmail.com>
2026-03-30 23:10:42 +0900

    Enhance flake.nix with virglrenderer support

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "QEMU from git master with PAT patch";
+  description = "QEMU from git master with virglrenderer from git, PAT patch, and full graphics support";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -8,7 +8,6 @@
       url = "git+https://gitlab.com/qemu-project/qemu.git";
       flake = false;
     };
-
     keycodemapdb = {
       url = "git+https://gitlab.com/qemu-project/keycodemapdb.git";
       flake = false;
@@ -21,34 +20,68 @@
       url = "git+https://gitlab.com/qemu-project/berkeley-testfloat-3.git";
       flake = false;
     };
+
+    virglrenderer-src = {
+      url = "gitlab:virgl/virglrenderer?host=gitlab.freedesktop.org";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, qemu-src, ... }@inputs:
+  outputs = { self, nixpkgs, qemu-src, virglrenderer-src, ... }@inputs:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
       overlays.default = final: prev: {
-        qemu = prev.qemu.overrideAttrs (oldAttrs: {
-          pname = "qemu-patched-git";
-          
-          # DYNAMIC VERSIONING:
-          # This creates a version like: "10.2.91-git-20260329-770f50c"
-          # It uses the base version, the date of the commit, and the short hash.
-          version = "${nixpkgs.lib.substring 0 8 qemu-src.lastModifiedDate}-${qemu-src.shortRev or "dirty"}";
 
+        # virglrenderer built from git HEAD
+        virglrenderer = prev.virglrenderer.overrideAttrs (oldAttrs: {
+          version = "git-${virglrenderer-src.shortRev or virglrenderer-src.rev}";
+          src = virglrenderer-src;
+        });
+
+        # QEMU built from git HEAD, using our git virglrenderer, with full graphics stack
+        qemu = (prev.qemu.override {
+          # Use our git virglrenderer
+          virglrenderer    = final.virglrenderer;
+
+          # Graphics / display
+          virglSupport     = true;
+          openGLSupport    = true;
+          sdlSupport       = true;
+          gtkSupport       = true;
+          vncSupport       = true;
+          spiceSupport     = true;
+          usbredirSupport  = true;
+
+          # Audio
+          pulseSupport     = true;
+          pipewireSupport  = true;
+          alsaSupport      = true;
+
+          # System / misc
+          numaSupport      = true;
+          seccompSupport   = true;
+          smartcardSupport = true;
+          tpmSupport       = true;
+          uringSupport     = true;
+          fuseSupport      = true;
+          capstoneSupport  = true;
+          libiscsiSupport  = true;
+        }).overrideAttrs (oldAttrs: {
+          pname = "qemu-patched-git";
+          version = "${nixpkgs.lib.substring 0 8 qemu-src.lastModifiedDate}-${qemu-src.shortRev or "dirty"}";
           src = qemu-src;
 
           patches = (oldAttrs.patches or [ ]) ++ [
             ./qemu-all-in-one.patch
           ];
 
-
           preConfigure = ''
             touch .git
             mkdir -p subprojects
-            
+
             cp -a ${inputs.keycodemapdb} subprojects/keycodemapdb
             cp -a ${inputs.berkeley-softfloat-3} subprojects/berkeley-softfloat-3
             cp -a ${inputs.berkeley-testfloat-3} subprojects/berkeley-testfloat-3
@@ -56,9 +89,8 @@
 
             cp -r subprojects/packagefiles/berkeley-softfloat-3/* subprojects/berkeley-softfloat-3/
             cp -r subprojects/packagefiles/berkeley-testfloat-3/* subprojects/berkeley-testfloat-3/
-            
-            rm -rf build/meson-private
 
+            rm -rf build/meson-private
           '';
 
           nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
@@ -87,16 +119,21 @@
           };
         in
         {
-          default = pkgs.qemu;
+          default       = pkgs.qemu;
+          qemu          = pkgs.qemu;
+          virglrenderer = pkgs.virglrenderer;
         });
 
       devShells = forAllSystems (system:
         let
-          pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
         in
         {
           default = pkgs.mkShell {
-            buildInputs = [ pkgs.qemu ];
+            buildInputs = [ pkgs.qemu pkgs.virglrenderer ];
           };
         });
     };
