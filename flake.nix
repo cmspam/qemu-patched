49286e6cb9844526bccfa22a9799cd0916b9a3d4
cmspam <charlesmillerspam@gmail.com>
2026-04-02 01:41:36 +0900

    Update virglrenderer version to 9.9.9

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
 
         # 1. Your custom virglrenderer (The 'Replacement')
         virglrenderer-git = prev.virglrenderer.overrideAttrs (oldAttrs: {
-          version = "git-${virglrenderer-src.shortRev or virglrenderer-src.rev}";
+          version = "9.9.9";
           src = virglrenderer-src;
           patches = (oldAttrs.patches or [ ]) ++ [
             ./virglrenderer-xe-native-context.patch
