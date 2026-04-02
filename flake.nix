3439a2fa3227e38f5b986b7744c3049dda718ac2
cmspam <charlesmillerspam@gmail.com>
2026-04-03 05:11:35 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
 
         # 1. Your custom virglrenderer (The 'Replacement')
         virglrenderer-git = prev.virglrenderer.overrideAttrs (oldAttrs: {
-          version = "7.9.4";
+          version = "7.9.5";
           src = virglrenderer-src;
           patches = (oldAttrs.patches or [ ]) ++ [
             ./virglrenderer-xe-native-context.patch
